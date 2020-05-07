c2caa023c088ea0c3e977edf0e347836e5ff84c5
albertolopes <alberto_llopes@hotmail.com>
2020-05-06 21:54:05 -0300

    Entidade Pedido. Datas com Instant e padrão ISO 8601

--- src/main/java/com/allos/course/entities/User.java
+++ src/main/java/com/allos/course/entities/User.java
@@ -1,12 +1,14 @@
 package com.allos.course.entities;
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name = "TB_USER")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -18,11 +20,14 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User() {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
-        super();
         this.id = id;
         this.name = name;
         this.email = email;
@@ -68,6 +73,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
---
+++ src/main/java/com/allos/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.allos.course.repositories;
+
+import com.allos.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
