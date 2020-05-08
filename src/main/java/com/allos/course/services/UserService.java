5cc1e78dc94d0a6c988ba926a1852099f3604e90
albertolopes <alberto_llopes@hotmail.com>
2020-05-08 19:49:40 -0300

    Deleção de User

--- src/main/java/com/allos/course/services/UserService.java
+++ src/main/java/com/allos/course/services/UserService.java
@@ -26,4 +26,8 @@ public class UserService {
     public User save(User obj){
         return repository.save(obj);
     }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
