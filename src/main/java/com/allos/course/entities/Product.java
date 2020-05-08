819eb3269e7f6a730bfd00add2f91cf8fb00e652
albertolopes <alberto_llopes@hotmail.com>
2020-05-08 17:59:10 -0300

    Associação muitos para muitos entre Product e OrderItem

--- src/main/java/com/allos/course/entities/Product.java
+++ src/main/java/com/allos/course/entities/Product.java
@@ -1,5 +1,7 @@
 package com.allos.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -29,6 +31,9 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "ID_CATEGORY")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){
 
@@ -84,6 +89,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x: items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
