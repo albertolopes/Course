454daf0a08300b3fdc345834d9803702371b9b5f
albertolopes <alberto_llopes@hotmail.com>
2020-05-08 23:14:05 -0300

    Exception User

--- src/main/java/com/allos/course/services/UserService.java
+++ src/main/java/com/allos/course/services/UserService.java
@@ -2,9 +2,14 @@ package com.allos.course.services;
 
 import com.allos.course.entities.User;
 import com.allos.course.repositories.UserRepository;
+import com.allos.course.services.exceptions.DatabaseException;
+import com.allos.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,9 +33,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getOne(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getOne(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
@@ -40,6 +49,12 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 }
